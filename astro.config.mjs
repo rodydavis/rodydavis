8c3f724c4ba03a0acd52339f66ef08d7783c57e6
Rody Davis <rody.davis.jr@gmail.com>
2023-02-09 11:49:37 -0800

    updating config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,43 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
-import remarkObsidian from "remark-obsidian";
 import { obsidianTags } from "./obsidian-tags.mjs";
-
-// https://astro.build/config
+import rehypePrettyCode from "rehype-pretty-code";
 import preact from "@astrojs/preact";
+
+const prettyCodeOptions = {
+  theme: {
+    light: "github-light",
+    dark: "github-dark",
+  },
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [
+        {
+          type: "text",
+          value: " ",
+        },
+      ];
+    }
+  },
+  onVisitHighlightedLine(node) {
+    node.properties.className.push("highlighted");
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ["word"];
+  },
+  tokensMap: {},
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rodydavis.com/",
   integrations: [sitemap(), preact({ compat: true })],
   markdown: {
+    // shikiConfig: { theme: "css-variables" },
     remarkPlugins: [rehypeAccessibleEmojis, obsidianTags],
     rehypePlugins: [
+      [rehypePrettyCode, prettyCodeOptions],
       "rehype-slug",
       [
         "rehype-autolink-headings",
@@ -21,7 +45,6 @@ export default defineConfig({
           behavior: "prepend",
         },
       ],
-      //   ["rehype-toc", { headings: ["h2", "h3"] }],
     ],
   },
 });
